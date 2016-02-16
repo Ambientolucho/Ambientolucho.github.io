7be98267622594528b494321eae9c0d24e4c870e
Luis Santamaría López <luisantamari@gmail.com>
2016-02-16 18:33:02 +0100

    js1

---
+++ js1/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Almacenar en una variable el resultado de sumar 1 y 2
+var a = 1 + 2;
+console.log(a);
+// alert(a); // Esto muestra una ventana emergente
+alert(a);
+// Almacenar en una variable el resultado de dividir 6 entre 2
+var b = 6 /2;
+console.log(b);
+// Almacenar en una variable el precio de un artículo de 20€ aplicándole el 21% de IVA .
+var c = 20 *1.21;
+console.log(c);
+// Definir una variable con el valor 4 y utilizar el operador (```++```) para incrementar en uno su valor.
+var d = 4;
+d++;
+console.log(d);
+// Definir una variable que almacene la concatenación de dos cadenas de texto.
+var text1 = 'Hola';
+var text2 = 'mundo';
+console.log(text1.concat(' ' +text2));
+// Definir una variable ```price``` con el valor ```19.99```y aplicar la abreviación ```/=``` para dividirlo entre ```1.21``` para obtener el precio sin IVA.
+var price = 19.99;
+price /=1.21;
+console.log(price);
+// Asignar a dos variables valores booleanos y hacer al menos una operación combinando un operador lógico: **AND** (```&&```) o **OR** (```||```)
+var valid = true;
+var invalid = false;
+var result = valid && invalid;
+console.log(result);
+// Realizar 4 expresiones que utilicen operadores relacionales (```<```, ```==```, ```!=``` y ```===```) y almacenen los valores en tres variables distintas.
